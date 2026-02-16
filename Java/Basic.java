749f9ddd0fe99bb2e609522ca2075ecfa5795dda
Keerthanagm-06 <krthana06@gmail.com>
2026-02-16 19:46:10 +0530

    Java folder modified

---
+++ Java/Basic.java
@@ -0,0 +1,16 @@
+public class Basic {
+    public static void main(String[] args) {
+        /* While loop - when you dont know how many types the loop is going to run means use while loop
+         int count = 1;
+        while(count != 5){
+            System.out.println(count);
+            count++;
+        }
+        */
+        /*For loop - when you know how many time the loop is going to run */
+        for(int count = 1; count !=5; count++){
+            System.out.println(count);
+        }
+
+    }
+}
---
+++ Java/Primitives.java
@@ -0,0 +1,12 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int roll = 64;
+        char letter = 'r';
+        float marks = 57.89f;
+        double largeDecimalNumbers = 4567654.4567;
+        long largeInteger = 345678345678765432L;
+        boolean check = false;
+
+        //primitives are something thst sny datatypes cannot break even futher.
+    }
+}
