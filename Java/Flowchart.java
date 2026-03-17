4d0f656ea56476b1ded78a18c72c24f9b0b2900e
Keerthanagmk-06 <krthana06@gmail.com>
2026-03-17 22:22:06 +0530

    ok

---
+++ Java/Flowchart.java
@@ -0,0 +1,49 @@
+public class Flowchart {
+    /* Q1: Input a number & print whether it is prime or not 
+     
+    Without Square root method 
+    start
+    input num
+    c =2
+    while c < num
+          if num % c == 0;
+               output "not prime"
+               exit
+          c = c + 1;
+               output "prime"
+               exit
+    end 
+
+    With Square root method
+
+    start
+    input n
+    if n <= 1
+      print("neither")
+    c = 2
+    while c * c < = n
+       if n % c == 0
+          output "not prime"
+          exit
+        c += 1
+    end while
+    output "prime"
+    exit
+    */
+
+    /* 
+    
+    Q2:Take input of a salary, if the salary if greater than n 10,000 add bonus as 2000,
+    otherwise add bobus as 1000
+
+     start
+     input salary;
+       if salary > 10000
+            salary = salary + 2000;
+       else
+            salary = salary + 1000;
+        output slarry;
+        exit
+
+    */
+}
